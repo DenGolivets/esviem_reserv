21f63b6483c425f701d68e4396c55102fc67f65a
DenGolivets <141760767+DenGolivets@users.noreply.github.com>
2025-07-28 23:34:04 +0300

    fix iphone

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,7 +71,7 @@ export const viewport = {
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
-  themeColor: "#4c4c4c",
+  themeColor: "#1d293d",
 };
 
 export const metadata: Metadata = {
@@ -106,11 +106,11 @@ export default function RootLayout({
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
-        />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-touch-fullscreen" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="theme-color" content="#1d293d" />
+        <meta name="msapplication-navbutton-color" content="#1d293d" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
