ef824b4771823e046d3a7e578824a764b6ce95ea
DenGolivets <141760767+DenGolivets@users.noreply.github.com>
2025-09-13 20:55:17 +0300

    add logos fix lang

--- utils/index.ts
+++ utils/index.ts
@@ -1,26 +1,50 @@
 export const partners = [
   {
     id: 1,
-    image: "/partners/sbu.svg",
-    width: "w-30", // стандартная ширина
-    height: "h-30", // стандартная высота
+    image: "/partners/asmap.png",
+    width: "w-40",
+    height: "h-40",
   },
   {
     id: 2,
-    image: "/partners/mvs.png",
-    width: "w-30", // стандартная ширина
-    height: "h-30", // стандартная высота
+    image: "/partners/everlast.png",
+    width: "w-40",
+    height: "h-40",
   },
   {
     id: 3,
-    image: "/partners/dsns.svg",
-    width: "w-30", // стандартная ширина
-    height: "h-30", // стандартная высота
+    image: "/partners/mhp.png",
+    width: "w-40",
+    height: "h-40",
   },
   {
     id: 4,
+    image: "/partners/saturn.png",
+    width: "w-30",
+    height: "h-30",
+  },
+  {
+    id: 5,
+    image: "/partners/kyivguma.png",
+    width: "w-60",
+    height: "h-60",
+  },
+  {
+    id: 6,
+    image: "/partners/namu.png",
+    width: "w-60",
+    height: "h-60",
+  },
+  {
+    id: 7,
+    image: "/partners/dsns.svg",
+    width: "w-30",
+    height: "h-30",
+  },
+  {
+    id: 8,
     image: "/partners/ink.png",
-    width: "w-48", // широкая для inkas
-    height: "h-40", // высокая для inkas
+    width: "w-48",
+    height: "h-40",
   },
 ];
